2dbefa0cc87d1a839944d4b6dbd24eb5144bc358
Veera Balarama Raju Datla <datla@achala.local>
2025-05-28 15:53:17 +0530

    VM

--- variable.tf
+++ variable.tf
@@ -29,37 +29,6 @@ variable "subnet_address_prefix" {
   type        = list(string)
 }
 
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "East US"
-}
-
-variable "vnet_name" {
-  description = "Name of the virtual network"
-  type        = string
-}
-
-variable "vnet_address_space" {
-  description = "VNet address space (e.g., 10.0.0.0/16)"
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet"
-  type        = string
-}
-
-variable "subnet_address_prefix" {
-  description = "Subnet address prefix (e.g., 10.0.1.0/24)"
-  type        = list(string)
-}
-
 variable "vm_name" {
   description = "Name of the Virtual Machine"
   type        = string
