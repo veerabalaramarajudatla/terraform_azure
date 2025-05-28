8ccaa11be16eea85a0004f9031a76e2600f86254
Veera Balarama Raju Datla <datla@achala.local>
2025-05-28 16:42:00 +0530

    removed sha

--- main.tf
+++ main.tf
@@ -60,11 +60,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
     azurerm_network_interface.nic.id,
   ]
 
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = file(var.ssh_public_key_path)
-  }
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
