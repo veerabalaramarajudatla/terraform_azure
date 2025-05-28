c56f3b19ebe152be7f9e0df4b47e1e475c0293a0
Veera Balarama Raju Datla <datla@achala.local>
2025-05-28 14:49:06 +0530

    Vnet Creation

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,6 @@
-resource_group_name = "my-rg-for-myresource"
-location            = "East US"
+resource_group_name   = "my-rg-for-myresource"
+location              = "East US"
+vnet_name             = "my-vnet"
+vnet_address_space    = "10.0.0.0/16"
+subnet_name           = "my-subnet"
+subnet_address_prefix = ["10.0.1.0/24"]
--- variable.tf
+++ variable.tf
@@ -1,10 +1,30 @@
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "Name of the resource group"
   type        = string
 }
 
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "East US"  # Optional default
+  default     = "East US"
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "VNet address space (e.g., 10.0.0.0/16)"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_address_prefix" {
+  description = "Subnet address prefix (e.g., 10.0.1.0/24)"
+  type        = list(string)
 }
