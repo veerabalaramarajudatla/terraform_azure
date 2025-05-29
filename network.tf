2ab248acc0508c9f348c8804dae110bede7808a6
Veera Balarama Raju Datla <datla@achala.local>
2025-05-29 18:35:50 +0530

    Removed 2 nd Vnet and Subnet

--- network.tf
+++ network.tf
@@ -1,50 +1,18 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name #Resource Group for this terraform infra deployment
-  location = var.location #Region for my Infra Setup
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name #Virtual Network 1 for the VM 1
-  address_space       = [var.vnet_address_space] #IP Address for the Virtual Network 1
-  location            = var.location #Region for my Infra Setup
-  resource_group_name = azurerm_resource_group.rg.name #My Resource Group
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name #Subnet 1 for my Virtual Network 1
-  resource_group_name = azurerm_resource_group.rg.name #My Resource Group
-  virtual_network_name = azurerm_virtual_network.vnet.name #Virtual Network 1
-  address_prefixes     = var.subnet_address_prefix #IP Address for my Subnet 1 (VM - 1)
-}
-
-resource "azurerm_virtual_network" "nvnet" {
-  name                = var.nvnet_name #Virtual Network 2 for the VM 2
-  address_space       = [var.nvnet_address_space] #IP Address for the Virtual Network 2
-  location            = var.location #Region for my Infra Setup
-  resource_group_name = azurerm_resource_group.rg.name #My Resource Group
-}
-
-resource "azurerm_subnet" "nsubnet" {
-  name                 = var.nsubnet_name #Subnet 1 for my virtual Network 2 
-  resource_group_name = azurerm_resource_group.rg.name #My Resource Group
-  virtual_network_name = azurerm_virtual_network.nvnet.name #Vitual Network 2
-  address_prefixes     = var.nsubnet_address_prefix #IP Address for the Subnet 1 (VM - 2)
-}
-
-resource "azurerm_virtual_network_peering" "vnet1-to-vnet2" {
-  name                      = "vnet1-to-vnet2"
-  resource_group_name = azurerm_resource_group.rg.name #My Resource Group Name
-  virtual_network_name      = azurerm_virtual_network.vnet.name #Virtual Network 1
-  remote_virtual_network_id = azurerm_virtual_network.nvnet.id #Virtual Network 2 as a remote
-  allow_forwarded_traffic      = true
-  allow_virtual_network_access = true
-}
-
-resource "azurerm_virtual_network_peering" "vnet2-to-vnet1" {
-  name                      = "vnet2-to-vnet1"
-  resource_group_name = azurerm_resource_group.rg.name #My Resource Group Name
-  virtual_network_name      = azurerm_virtual_network.nvnet.name #Virtual Network 2
-  remote_virtual_network_id = azurerm_virtual_network.vnet.id #Virtual Network 1 as a remote
-  allow_forwarded_traffic      = true
-  allow_virtual_network_access = true
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefix
 }
--- nic.tf
+++ nic.tf
@@ -1,33 +1,33 @@
 resource "azurerm_network_interface" "nic1" {
-  name                = "${var.vm_name}-nic"            # Network Interface 1
-  location            = var.location                    # Region for My Infra
-  resource_group_name = azurerm_resource_group.rg.name         # My Resource Group
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id    # Connected to Subnet 1
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_network_interface" "nic2" {
-  name                = "${var.nvm_name}-nic"           # Network Interface 2
-  location            = var.location                    # Region for My Infra
-  resource_group_name = azurerm_resource_group.rg.name        # My Resource Group
+  name                = "${var.nvm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.nsubnet.id   # Connected to Subnet 2
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_network_interface_security_group_association" "nic1_assoc" {
-  network_interface_id      = azurerm_network_interface.nic1.id   # NIC1 id
-  network_security_group_id = azurerm_network_security_group.nsg.id # NSG id
+  network_interface_id      = azurerm_network_interface.nic1.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_network_interface_security_group_association" "nic2_assoc" {
-  network_interface_id      = azurerm_network_interface.nic2.id   # NIC2 id
-  network_security_group_id = azurerm_network_security_group.nsg.id # NSG id
+  network_interface_id      = azurerm_network_interface.nic2.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
