84b71bdb6076fcf594bb389a8fab6b70afeed1b1
Veera Balarama Raju Datla <datla@achala.local>
2025-05-29 13:42:51 +0530

    New Vnet and New Subnet

--- variable.tf
+++ variable.tf
@@ -10,22 +10,22 @@ variable "location" {
 }
 
 variable "vnet_name" {
-  description = "Name of the Virtual Network"
+  description = "Name of the first Virtual Network"
   type        = string
 }
 
 variable "vnet_address_space" {
-  description = "Address space for the Virtual Network"
+  description = "Address space for the first Virtual Network"
   type        = string
 }
 
 variable "subnet_name" {
-  description = "Name of the Subnet"
+  description = "Name of the first Subnet"
   type        = string
 }
 
 variable "subnet_address_prefix" {
-  description = "Address prefix for the Subnet"
+  description = "Address prefix for the first Subnet"
   type        = list(string)
 }
 
@@ -48,4 +48,24 @@ variable "admin_user_name" {
 variable "ssh_key" {
   description = "Path to SSH public key file (relative to Terraform root)"
   type        = string
+}
+
+variable "nvnet_name" {
+  description = "Name of the second Virtual Network"
+  type        = string
+}
+
+variable "nvnet_address_space" {
+  description = "Address space for the second Virtual Network"
+  type        = string
+}
+
+variable "nsubnet_name" {
+  description = "Name of the second Subnet"
+  type        = string
+}
+
+variable "nsubnet_address_prefix" {
+  description = "Address prefix of the second Subnet"
+  type        = list(string)
 }
